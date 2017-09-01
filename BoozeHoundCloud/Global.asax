9d526c5fecff16326a9bf8406b8aecbd3082cb49
GraemeB <grae22@gmail.com>
2017-09-01 17:10:20 +0200

    Adding MVC project.

---
+++ BoozeHoundCloud/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BoozeHoundCloud.MvcApplication" Language="C#" %>
---
+++ BoozeHoundCloud/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BoozeHoundCloud.Startup))]
+namespace BoozeHoundCloud
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
