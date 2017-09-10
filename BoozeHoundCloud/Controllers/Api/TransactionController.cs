2b723f912c6b3d7d7540f2afab5e5293befc7172
graeme <grae22@gmail.com>
2017-09-10 21:11:14 +0200

    [WIP] TransactionController.

---
+++ BoozeHoundCloud/Controllers/Api/TransactionController.cs
@@ -0,0 +1,41 @@
+﻿using System.Web.Http;
+using BoozeHoundCloud.DataAccess;
+using BoozeHoundCloud.Models;
+using BoozeHoundCloud.Models.Core;
+using BoozeHoundCloud.Services;
+
+namespace BoozeHoundCloud.Controllers.Api
+{
+  public class TransactionController : ApiController
+  {
+    //-------------------------------------------------------------------------
+
+    private ITransactionService _transactions;
+
+    //-------------------------------------------------------------------------
+
+    public TransactionController()
+    {
+      var context = new ApplicationDbContext();
+      var transactionRepository = new GenericRepository<Transaction>(context);
+      var accountRepository = new GenericRepository<Account>(context);
+      var accountTypeRepository = new GenericRepository<AccountType>(context);
+      var accountService = new AccountService(accountRepository, accountTypeRepository);
+
+      _transactions =
+        new TransactionService(
+          context,
+          transactionRepository,
+          accountService);
+    }
+
+    //-------------------------------------------------------------------------
+
+    public TransactionController(ITransactionService transactions)
+    {
+      _transactions = transactions;
+    }
+
+    //-------------------------------------------------------------------------
+  }
+}
---
+++ BoozeHoundCloud_Test/Controllers/Api/TransactionController_Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using NUnit.Framework;
+using Moq;
+using BoozeHoundCloud;
+using BoozeHoundCloud.Controllers.Api;
+using BoozeHoundCloud.Services;
+
+namespace BoozeHoundCloud_Test.Controllers.Api
+{
+  [TestFixture]
+  [Category("TransactionController")]
+  internal class TransactionController_Test
+  {
+    //-------------------------------------------------------------------------
+
+    private TransactionController _testObject;
+    private Mock<ITransactionService> _transactions;
+
+    //-------------------------------------------------------------------------
+
+    [SetUp]
+    public void SetUp()
+    {
+      AutoMapperConfig.Initialise();
+
+      _transactions = new Mock<ITransactionService>();
+
+      _testObject = new TransactionController(_transactions.Object);
+
+      _testObject.Request = new HttpRequestMessage(new HttpMethod("POST"), new Uri("http://localhost"));
+    }
+
+    //-------------------------------------------------------------------------
+  }
+}
