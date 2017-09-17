30a4ab1921fad18fcedc267b52ddc59e170e7b09
graeme <grae22@gmail.com>
2017-09-17 22:01:14 +0200

    [WIP] Adding & updating accounts.

--- BoozeHoundCloud/Areas/Core/Controllers/AccountController.cs
+++ BoozeHoundCloud/Areas/Core/Controllers/AccountController.cs
@@ -1,6 +1,11 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
+using System.Web.Routing;
+using AutoMapper;
+using BoozeHoundCloud.Areas.Core.DataTransferObjects;
 using BoozeHoundCloud.Areas.Core.Models;
+using BoozeHoundCloud.Areas.Core.Services;
+using BoozeHoundCloud.Areas.Core.ViewModels;
 using BoozeHoundCloud.Models;
 
 namespace BoozeHoundCloud.Areas.Core.Controllers
@@ -10,18 +15,20 @@ namespace BoozeHoundCloud.Areas.Core.Controllers
     //-------------------------------------------------------------------------
 
     private readonly IApplicationDbContext _context;
+    private readonly IAccountService _accountService;
 
     //-------------------------------------------------------------------------
 
     public AccountController()
     {
       _context = new ApplicationDbContext();
+      _accountService = AccountService.Create(_context);
     }
 
     //-------------------------------------------------------------------------
 
     // GET: Core/Account
-    [System.Web.Mvc.HttpGet]
+    [HttpGet]
     public ActionResult Index(string typeName)
     {
       return View("Index");
@@ -29,7 +36,8 @@ namespace BoozeHoundCloud.Areas.Core.Controllers
 
     //-------------------------------------------------------------------------
 
-    [System.Web.Mvc.HttpGet]
+    // GET: Core/Account/AccountsOfType?typeName=X
+    [HttpGet]
     public ActionResult AccountsOfType(string typeName)
     {
       AccountType accountType = _context.AccountTypes.FirstOrDefault(x => x.Name.Equals(typeName));
@@ -40,6 +48,55 @@ namespace BoozeHoundCloud.Areas.Core.Controllers
       }
 
       return View(accountType);
+    }
+
+    //-------------------------------------------------------------------------
+
+    // GET: Core/Account/Create
+    [HttpGet]
+    public ActionResult Create()
+    {
+      var viewModel = new AccountFormViewModel
+      {
+        AccountTypes = _context.AccountTypes
+      };
+
+      return View("AccountForm", viewModel);
+    }
+
+    //-------------------------------------------------------------------------
+
+    // GET: Core/Account/Save
+    [ValidateAntiForgeryToken]
+    public ActionResult Save(Account account)
+    {
+      if (ModelState.IsValid == false)
+      {
+        return Create();
+      }
+
+      bool isNewAccount = (account.Id == 0);
+
+      account.AccountType = _context.AccountTypes.FirstOrDefault(x => x.Id == account.AccountTypeId);
+
+      if (account.AccountType == null)
+      {
+        return Create();
+      }
+
+      if (isNewAccount)
+      {
+        var accountDto = Mapper.Map<Account, AccountDto>(account);
+        _accountService.AddAccount(accountDto);
+      }
+      else
+      {
+        // TODO: Update existing account.
+      }
+
+      var args = new RouteValueDictionary();
+      args.Add("typeName", account.AccountType.Name);
+      return RedirectToAction("AccountsOfType", "Account", args);
     }
 
     //-------------------------------------------------------------------------
---
+++ BoozeHoundCloud/Areas/Core/ViewModels/AccountFormViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using BoozeHoundCloud.Areas.Core.Models;
+
+namespace BoozeHoundCloud.Areas.Core.ViewModels
+{
+  public class AccountFormViewModel
+  {
+    //-------------------------------------------------------------------------
+
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(Account.NameMaxLength)]
+    public string Name { get; set; }
+
+    [Required]
+    [DisplayName("Account Type")]
+    public int AccountTypeId { get; set; }
+
+    public IEnumerable<AccountType> AccountTypes { get; set; }
+
+    public decimal Balance { get; set; }
+
+    //-------------------------------------------------------------------------
+  }
+}
