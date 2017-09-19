12f1c65755cc76706db294342b038c2b27ddf073
graeme <grae22@gmail.com>
2017-09-19 22:13:14 +0200

    Added TransactionFormViewModel.

---
+++ BoozeHoundCloud/Areas/Core/ViewModels/TransactionFormViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using BoozeHoundCloud.Areas.Core.Models;
+
+namespace BoozeHoundCloud.Areas.Core.ViewModels
+{
+  public class TransactionFormViewModel
+  {
+    //-------------------------------------------------------------------------
+
+    public int Id { get; set; }
+
+    public decimal Value { get; set; }
+
+    public int DebitAccountId { get; set; }
+
+    public int CreditAccountId { get; set; }
+
+    [MaxLength(Transaction.ReferenceMaxLength)]
+    public string Reference { get; set; }
+
+    [MaxLength(Transaction.DescriptionMaxLength)]
+    public string Description { get; set; }
+
+    public DateTime Date { get; set; }
+
+    //-------------------------------------------------------------------------
+  }
+}
