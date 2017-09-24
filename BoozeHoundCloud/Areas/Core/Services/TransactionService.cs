bc82ce19a7e442c4f5c3103d9209aad65b732607
graeme <grae22@gmail.com>
2017-09-24 21:13:06 +0200

    Another stab at the timestamp compare.

--- BoozeHoundCloud/Areas/Core/Services/TransactionService.cs
+++ BoozeHoundCloud/Areas/Core/Services/TransactionService.cs
@@ -99,7 +99,7 @@ namespace BoozeHoundCloud.Areas.Core.Services
         throw new BusinessLogicException("Transaction date cannot change.");
       }
 
-      if ((modifiedTransaction.CreatedTimestamp - orignalTransaction.CreatedTimestamp).Ticks != 0)
+      if ((int)(modifiedTransaction.CreatedTimestamp - orignalTransaction.CreatedTimestamp).TotalSeconds != 0)
       {
         throw new BusinessLogicException("Transaction created-timestamp cannot change.");
       }
